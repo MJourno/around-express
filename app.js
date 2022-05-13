08821c3f1f9d469767ea6037c7d92374bcace642
Maayan Journo <maayanjourno@gmail.com>
2022-05-14 00:02:21 +0300

    1

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
+
 const app = express();
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
-
 const { PORT = 3000 } = process.env;
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('hello world');
 });
 
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
-
 app.listen(PORT, () => {
-  console.log(`App listen on port ${PORT}...`)
+  console.log(`App listen on port ${PORT}...`);
 });
--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,16 @@
-const { getJsonFromFile } = require('../helpers/files')
 const path = require('path');
 
-const usersFilePath = path.join(__dirname, '..', 'data', 'users.json')
+const { getJsonFromFile } = require('../helpers/files');
+
+const usersFilePath = path.join(__dirname, '..', 'data', 'users.json');
 
 const getUsers = async (req, res) => {
   try {
     const users = await getJsonFromFile(usersFilePath);
 
-    res.send(users)
+    res.send(users);
   } catch (error) {
-    console.log('Error happened in getUsers', error)
+    console.log('Error happened in getUsers', error);
     res.status(500).send('Something went wrong');
   }
 };
@@ -17,7 +18,7 @@ const getUsers = async (req, res) => {
 const getUserById = async (req, res) => {
   try {
     const users = await getJsonFromFile(usersFilePath);
-    const user = users.find(user => user._id === req.params.user_id)
+    const user = users.find(user => user._id === req.params.user_id);
 
     if (!user) {
       res.status(404).send('Not Found');
@@ -25,12 +26,12 @@ const getUserById = async (req, res) => {
     res.send(user);
 
   } catch (error) {
-    console.log('Error happened in getUserById', error)
+    console.log('Error happened in getUserById', error);
     res.status(500).send('Something went wrong');
   }
 }
 
 module.exports = {
   getUsers,
-  getUserById
-}
+  getUserById,
+};
